0da53bbc5c9702f89774dd3859cc5006aa985c94
Artur Rocha <adsr@poczta.onet.pl>
2024-12-30 13:26:06 +0100

    Quick and dirty ebook info

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"archive/zip"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"os"
 )
+
+type Metadata struct {
+	XMLName  xml.Name `xml:"package"`
+	Metadata struct {
+		Title   string   `xml:"title"`
+		Creator []string `xml:"creator"`
+	} `xml:"metadata"`
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -18,7 +27,57 @@ func main() {
 	}
 	defer zipFile.Close()
 
-	for _, f := range zipFile.File {
-		fmt.Println(f.Name)
+	var metadata Metadata
+
+	var container struct {
+		Rootfiles struct {
+			Rootfile struct {
+				FullPath string `xml:"full-path,attr"`
+			} `xml:"rootfile"`
+		} `xml:"rootfiles"`
 	}
+
+	for _, f := range zipFile.File {
+		if f.Name == "META-INF/container.xml" {
+			rc, err := f.Open()
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer rc.Close()
+
+			// Parse container.xml to find the path to the OPF file
+			if err := xml.NewDecoder(rc).Decode(&container); err != nil {
+				log.Fatal(err)
+			}
+
+			break
+		}
+	}
+
+	// Find and parse the OPF file
+	for _, f := range zipFile.File {
+		if f.Name == container.Rootfiles.Rootfile.FullPath {
+			rc, err := f.Open()
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer rc.Close()
+
+			if err := xml.NewDecoder(rc).Decode(&metadata); err != nil {
+				log.Fatal(err)
+			}
+
+			break
+		}
+	}
+
+	fmt.Printf("Title: %s\n", metadata.Metadata.Title)
+	fmt.Printf("Author(s): ")
+	for i, author := range metadata.Metadata.Creator {
+		if i > 0 {
+			fmt.Printf(", ")
+		}
+		fmt.Printf("%s", author)
+	}
+	fmt.Println()
 }
